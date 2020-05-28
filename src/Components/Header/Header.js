1e603dd359dc74b74910c993912d2f09126f9211
Justin <justindorman6@gmail.com>
2020-05-28 19:25:27 +0200

    Services and profile section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import classes from './Header.module.css'
 import Logo from '../../assets/images/mdLogo.png'
+import Services from '../ServicesSection/ServicesSection';
+
 
 const Header = () => {
     return (
         <header className={classes.Header}>
             <img src={Logo} alt="logo" />
-            <button className={classes.Button}>Contact Us</button>
+
+            <button className={classes.Button}>Services offered</button>
+            <button className={classes.Button}>Profile</button>
+            <button className={classes.Button}>Before&After</button>
+            <button className={classes.Contact}>Contact Us</button>
         </header>
     )
 }
