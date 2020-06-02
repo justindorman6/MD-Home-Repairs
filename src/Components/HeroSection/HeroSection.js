4de62d99b6ff298ea3cd973f477fee4b7cfe4e1c
Zachary <zbresler16@gmail.com>
2020-06-02 21:54:29 +0200

    style: hero image and black overlay

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import classes from './HeroSection.module.css'
+import HeroImage from '../../assets/images/tools.jpg'
 
 const HeroSection = (props) => {
     return (
         <div className={classes.HeroSectionContainer}>
-            <h1>MD Home Repairs</h1>
-            {props.mobile ? <p>The best service money can buy.</p> :
-                <p>Providing you with the best service money can buy.</p>}
+            <img src={HeroImage} alt="hero"/>
+            <div className={classes.Overlay}></div>
+            <div className={classes.Headings}>
+                <h1>MD Home Repairs</h1>
+                {props.mobile ? <p>The best service money can buy.</p> :
+                    <p>Providing you with the best service money can buy.</p>}
+            </div>
 
         </div>
     )
